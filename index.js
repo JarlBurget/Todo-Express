352dd90e32a009b81311c1b36792cc0ae2b412af
jarl <jarl-ivor.burget@voco.ee>
2024-11-06 13:06:28 +0200

    Write data to file

--- index.js
+++ index.js
@@ -7,16 +7,47 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf-8', (error, data) => {
-        if (error) {
-            console.error(error)
-            return 
-        } 
+app.use(express.urlencoded({extended: true}))
+
+const readFile = (filename) => {
+    return new Promise ((resolve, reject) => {
+        fs.readFile(filename, 'UTF-8', (error,data) => {
+        if(err) {
+            console.error(error);
+            return
+        }
         const tasks = data.split('\n')
-        res.render('index', {tasks: tasks})
-    })
+        resolve(tasks)
+    });
 })
+} 
+
+
+app.get('/', (req, res) => {
+    readFile('/tasks')
+    .then((tasks) => {
+        console.log(tasks)
+        res.render('index', {tasks: tasks})
+    })   
+})
+
+app.use(express.urlencoded({ extended: true}));
+
+app.post('/', (req, res)=> {
+    readFile('/tasks')
+    .then((tasks) => {
+        tasks.push(req.body.task)
+        const content = tasks.join('\n')
+        fs.writeFile('./tasks', data, err=>{
+            if (err) {
+                console.error(err);
+                return
+            } 
+            res.redirect('/')
+        })
+    })   
+})
+
 
 app.listen(3001, () => {
     console.log('Server started at http://localhost:3001')
