0e9ea8d957b65d5f853fc535836a3411a45dddec
jarl <jarl-ivor.burget@voco.ee>
2024-11-20 14:05:33 +0200

    Kustutamine

--- index.js
+++ index.js
@@ -12,43 +12,86 @@ app.use(express.urlencoded({extended: true}))
 const readFile = (filename) => {
     return new Promise ((resolve, reject) => {
         fs.readFile(filename, 'UTF-8', (error,data) => {
-        if(err) {
+        if(error) {
             console.error(error);
             return
         }
-        const tasks = data.split('\n')
+        const tasks = JSON.parse(data)
         resolve(tasks)
-    });
+    })
 })
+} 
+
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) =>{
+        fs.writeFile(filename, data, 'utf-8', err =>{
+            if (err){
+                console.error(err);
+                return
+            } 
+            resolve(true)
+        } )
+    } )
 } 
 
 
 app.get('/', (req, res) => {
-    readFile('/tasks')
+    readFile('./tasks.json')
     .then((tasks) => {
         console.log(tasks)
         res.render('index', {tasks: tasks})
     })   
 })
 
-app.use(express.urlencoded({ extended: true}));
-
-app.post('/', (req, res)=> {
-    readFile('/tasks')
+app.post('/', (req, res) => {
+    readFile('./tasks.json')
     .then((tasks) => {
-        tasks.push(req.body.task)
-        const content = tasks.join('\n')
-        fs.writeFile('./tasks', data, err=>{
-            if (err) {
-                console.error(err);
-                return
-            } 
-            res.redirect('/')
-        })
-    })   
+        let index
+        if(tasks.length === 0)
+        {
+            index = 0
+        } else {
+            index = tasks[tasks.length - 1].id + 1;  
+        } 
+        const newTask = {
+            "id" : index,
+            "task" : req.body.task
+        } 
+
+        
+        tasks.push(newTask)
+        data = JSON.stringify(tasks, null, 2)
+        writeFile('./tasks.json', data)
+        res.redirect('/')
+
+    })
 })
 
+app.get('/delete-task/:taskId', (req, res) =>{
+    let deletedTaskId = parseInt(req.params.taskId)
+    readFile('./tasks.json')
+    .then(tasks =>{
+        tasks.forEach((task, index) =>{
+            if(task.id === deletedTaskId) {
+                tasks.splice(index, 1)
+            } 
+        })
+        data = JSON.stringify(tasks, null, 2)
+        writeFile('./tasks.json', data)
+            res.redirect('/')
+        })
+    })
+
+
+app.get('/delete-tasks', (req, res) =>{
+    const data = JSON.stringify([] , null, 2)
+    writeFile('./tasks.json', data)
+    res.redirect('/')
+})
+   
 
 app.listen(3001, () => {
     console.log('Server started at http://localhost:3001')
 });
+
+
