4b037262656573a01a50872830357dbfd6e765a5
jarl <jarl-ivor.burget@voco.ee>
2024-11-22 14:02:50 +0200

    Update single task data

--- index.js
+++ index.js
@@ -4,105 +4,162 @@ const fs = require('node:fs');
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({extended: true}))
-
 const readFile = (filename) => {
-    return new Promise ((resolve, reject) => {
-        fs.readFile(filename, 'UTF-8', (error,data) => {
-        if(error) {
-            console.error(error);
-            return
-        }
-        const tasks = JSON.parse(data)
-        resolve(tasks)
-    })
-})
-} 
-
-const writeFile = (filename, data) => {
-    return new Promise((resolve, reject) =>{
-        fs.writeFile(filename, data, 'utf-8', err =>{
-            if (err){
-                console.error(err);
-                return
-            } 
-            resolve(true)
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            const tasks = JSON.parse(data) 
+            resolve(tasks)
         });
     })
 } 
 
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, 'utf8', err => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            resolve(true)
+        });
+    })
+}
 
 app.get('/', (req, res) => {
     readFile('./tasks.json')
-    .then(tasks => {
-        res.render('index', {
-            tasks: tasks,
-            error: null
-        })
-    })  
-}) 
-    
-
-app.post('/', (req, res) => {
-    let error = null
-    if(req.body.task.trim().length == 0){
-        error = 'Please insert correct task data'
-        readFile('./tasks.json')
-        .then(tasks => {
+        .then((tasks) => { 
             res.render('index', {
                 tasks: tasks,
-                error: error,
+                error: null
             })
-        })
+    })   
+})
 
-    } else {
+app.post('/', (req, res) => {
+    console.log('form sent data')
+    let task = req.body.task
+    let error = null
+    if(task.trim().length === 0){
+        error = 'Please insert correct task data'
+        readFile('./tasks.json')
+        .then((tasks) => { 
+            res.render('index', {
+                tasks: tasks, 
+                error: error
+            })
+        }) 
+    } else{
         readFile('./tasks.json')
         .then((tasks) => {
             let index
-            if(tasks.length === 0)
-            {
+            if(tasks.length === 0){
                 index = 0
             } else {
-                index = tasks[tasks.length - 1].id + 1;  
+                index = tasks[tasks.length - 1].id + 1 
             } 
+
             const newTask = {
-                "id" : index,
-                "task" : req.body.task
+                id: index,
+                task: task
             } 
+
             tasks.push(newTask)
-            data = JSON.stringify(tasks, null, 2)
+            console.log(tasks)
+
+            const data = JSON.stringify(tasks, null, 2)
+
             writeFile('./tasks.json', data)
+
             res.redirect('/')
         })
-    }
-    })
+    } 
 
-app.get('/delete-task/:taskId', (req, res) =>{
+})
+
+app.get('/delete-task/:taskId', (req, res) => {
     let deletedTaskId = parseInt(req.params.taskId)
     readFile('./tasks.json')
-    .then(tasks =>{
-        tasks.forEach((task, index) =>{
-            if(task.id === deletedTaskId) {
-                tasks.splice(index, 1)
-            } 
-        })
-        data = JSON.stringify(tasks, null, 2)
-        writeFile('./tasks.json', data)
-        res.redirect('/')
+        .then(tasks => {
+            tasks.forEach((task, index) => {
+                if(task.id === deletedTaskId){
+                    tasks.splice(index, 1)
+                } 
+            });
+
+            const data = JSON.stringify(tasks, null, 2)
+
+            writeFile('./tasks.json', data)
+
+            res.redirect('/')
         })
 })
 
+app.get('/update-task/:taskId', (req, res) => {
+    let updateTaskId = parseInt(req.params.taskId)
+    readFile('./tasks.json')
+        .then(tasks => {
+            let updateTask
+            tasks.forEach((task) => {
+                if(task.id === updateTaskId){
+                   updateTask = task.task 
+                } 
+            });
 
-app.get('/delete-tasks', (req, res) =>{
-    const data = JSON.stringify([] , null, 2)
+            res.render('update',{
+                updateTask: updateTask,
+                updateTaskId: updateTaskId,
+                error: null
+            } )
+        })
+})
+
+app.post('/update-task', (req, res) => {
+    console.log(req.body)
+    let updateTaskId = parseInt(req.body.taskId)
+    let updateTask =  req.body.task
+    let error = null
+    if(updateTask.trim().length === 0){
+        error = 'Please insert correct task data'
+        res.render('update',{
+            updateTask: updateTask,
+            updateTaskId: updateTaskId,
+            error: error
+        }) 
+    } else {
+        readFile('./tasks.json')
+        .then(tasks => {
+            tasks.forEach((task, index) => {
+                if(task.id === updateTaskId){
+                   tasks[index].task = updateTask
+                } 
+            });
+            console.log(tasks)
+            const data = JSON.stringify(tasks, null, 2)
+
+            writeFile('./tasks.json', data)
+
+            res.redirect('/')
+        }) 
+    } 
+  
+})
+
+app.get('/delete-tasks', (req, res) => {
+    tasks = [] 
+    const data = JSON.stringify(tasks, null, 2)
     writeFile('./tasks.json', data)
     res.redirect('/')
 })
-   
 
 app.listen(3001, () => {
-    console.log('Server started at http://localhost:3001')
+    console.log('Server is started http://localhost:3001')
 })
